3f51611fcea315c4429f2307b2a39ee642d1f81e
Tamima-Azad <165451442+Tamima-Azad@users.noreply.github.com>
2024-12-19 23:34:40 +0600

    Thursday

---
+++ week8/day5/B_Kevin_and_Permutation.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define getbit(n, i) (((n) & (1LL << (i))) != 0) 
+#define setbit0(n, i) ((n) & (~(1LL << (i)))) 
+#define setbit1(n, i) ((n) | (1LL << (i))) 
+#define togglebit(n, i) ((n) ^ (1LL << (i))) 
+#define lastone(n) ((n) & (-(n))) 
+char gap = 32;
+#define int long long
+#define ll long long 
+#define lll __int128_t
+#define pb push_back
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll hashPrime = 1610612741;
+void solve() {
+    int n,k;cin>>n>>k;
+    vector<int>p(n+1);
+    int c=1;
+   // p[k]=1;
+    int l=1,r=n-k;
+   while((n>=l)){
+    if(l%k==0)
+        p[l]=c++;
+
+        l++;
+    }
+
+    for(int i=n;i>=1;i--){
+        if(p[i]==0){
+            p[i]=c++;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        cout<<p[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
