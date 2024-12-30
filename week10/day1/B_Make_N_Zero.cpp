b7595ac29c839bfe58c06602d6b7d335ad6b5758
Tamima-Azad <165451442+Tamima-Azad@users.noreply.github.com>
2024-12-30 22:23:33 +0600

    monday

---
+++ week10/day1/B_Make_N_Zero.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define getbit(n, i) (((n) & (1LL << (i))) != 0) 
+#define setbit0(n, i) ((n) & (~(1LL << (i)))) 
+#define setbit1(n, i) ((n) | (1LL << (i))) 
+#define togglebit(n, i) ((n) ^ (1LL << (i))) 
+#define lastone(n) ((n) & (-(n))) 
+char gap = 32;
+#define int long long
+#define ll long long 
+#define lll __int128_t
+#define pb push_back
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll hashPrime = 1610612741;
+void solve() {
+    int n;cin>>n;
+    // int fr =n%4;
+    // int tre=n%3;
+    // if(n%3==0 or n%4==0 or (n%3+n%4)%3==0 or (n%3+n%4)%4==0) cout<<"YES\n";
+    // else cout<<"NO\n";
+    int f=false;
+    while(n>=0){
+        if(n%3==0 or n%4==0){
+            f=true;
+            break;
+        }
+        n-=4;
+    } 
+    if(f==true) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ week10/day1/C_Maximum_Score.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define getbit(n, i) (((n) & (1LL << (i))) != 0) 
+#define setbit0(n, i) ((n) & (~(1LL << (i)))) 
+#define setbit1(n, i) ((n) | (1LL << (i))) 
+#define togglebit(n, i) ((n) ^ (1LL << (i))) 
+#define lastone(n) ((n) & (-(n))) 
+char gap = 32;
+#define int long long
+#define ll long long 
+#define lll __int128_t
+#define pb push_back
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll hashPrime = 1610612741;
+void solve() {
+    int n;cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    } ll sr=0;
+    // for(int i=0;i<n;i+=2){
+    //     sr+=abs(v[i]-v[i+1]);
+    // }
+    // cout<<sr<<endl;
+    sort(v.begin(), v.end());
+
+        int score = 0;
+        for (int i = 0; i < n / 2; ++i) {
+            // Maximize absolute difference by pairing smallest with largest remaining elements
+            score += abs(v[i] - v[n - i - 1]);
+        }
+        cout << score << endl;
+
+    
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
